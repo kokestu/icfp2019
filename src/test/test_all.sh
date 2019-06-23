bcedd4d11e0df3106d1e629573e4cee023cf1751
Jonathan <jonny.a.sutton@gmail.com>
2019-06-23 15:32:13 +0100

    update testing

--- src/test/test_all.sh
+++ src/test/test_all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for file in /mnt/c/Users/Jonathan/Documents/code/icfp2019/part-1-initial/*
+for file in /mnt/c/Users/Jonathan/Documents/code/icfp2019/part-1-initial/*.desc
 do
-  python3 "$1" "$file"
+  python3 /mnt/c/Users/Jonathan/Documents/code/icfp2019/src/test/test_strategy.py "$file" "$1" "$2"
 done
--- src/test/test_strategy.py
+++ src/test/test_strategy.py
@@ -4,22 +4,35 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
 from map_io import read_input, write_output
-from strategy import RandomMovesStrategy
+from strategy import RandomMovesStrategy, ToAndFroStrategy
 
 import sys
+
+class InvalidStrategyException(Exception):
+    pass
+
+def get_strategy(str):
+    if str == 'random':
+        return RandomMovesStrategy
+    elif str == 'to_and_fro':
+        return ToAndFroStrategy
+    else:
+        raise InvalidStrategyException(str)
 
 def main():
     try:
         filename = sys.argv[1]
+        strategy_str = sys.argv[2]
     except IndexError:
-        print('Program requires input file!')
+        print('Program requires input file and strategy!')
     try:
-        draw_map = sys.argv[2] == '--draw'
+        draw_map = sys.argv[3] == '--draw'
     except IndexError:
         draw_map = False
+    strat = get_strategy(strategy_str)
     map = read_input(filename)
     print('{}:'.format(os.path.basename(filename)))
-    s = RandomMovesStrategy(map)
+    s = strat(map)
     s.solve_map()
     if draw_map:
         map._draw_map()
